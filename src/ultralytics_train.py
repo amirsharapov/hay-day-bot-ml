9fb75d79ec07013459653699ea98c5190615ddcd
amirsharapov <amirsharapov.personal@gmail.com>
2025-01-10 20:54:45 -0600

    pushing small version instead of nano

--- src/ultralytics_train.py
+++ src/ultralytics_train.py
@@ -4,8 +4,8 @@ from ultralytics import YOLO
 from src.dataset import Dataset
 
 
-def train_model(dataset: Dataset):
-    model = YOLO('yolo11n-seg.pt')
+def train_model(dataset: Dataset, model: str = 'yolo11s-seg.pt'):
+    model = YOLO(model)
     model.train(
         data=str(dataset.ultralytics_train_config.absolute()),
         epochs=50,
