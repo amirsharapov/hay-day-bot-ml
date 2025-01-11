3467ae4a2651356d032ac26089d78d402944d74b
amirsharapov <amirsharapov.personal@gmail.com>
2025-01-10 20:57:03 -0600

    smaller epochs

--- src/ultralytics_train.py
+++ src/ultralytics_train.py
@@ -4,10 +4,14 @@ from ultralytics import YOLO
 from src.dataset import Dataset
 
 
-def train_model(dataset: Dataset, model: str = 'yolo11s-seg.pt'):
+def train_model(
+        dataset: Dataset,
+        model: str = 'yolo11s-seg.pt',
+        epochs: int = 25
+):
     model = YOLO(model)
     model.train(
         data=str(dataset.ultralytics_train_config.absolute()),
-        epochs=50,
+        epochs=epochs,
         device=0 if torch.cuda.is_available() else 'cpu'
     )
